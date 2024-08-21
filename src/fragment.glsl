191795048e6abcf554db48766302e687010c1e28
dominic-chang <dochang@g.harvard.edu>
2024-08-21 19:24:25 -0400

    Remove dead code

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -30,8 +30,6 @@ vec2 c_pow(vec2 x, float y){
     return vec2(mag*cos(angle), mag*sin(angle));
 }
 
-
-
 float DRF(float X, float Y, float Z){
 
     float ERRTOL = pow(4.0*D1MACH3, 1.0/6.0);
@@ -104,142 +102,6 @@ float F(float phi, float m){
     return rawF(sin(phi), m);
 }
 
-float am(float u, float m, float tol){
-
-    float ambuf[10];
-    if(u == 0.){return 0.;}
-
-    float sqrt_tol = sqrt(tol);
-    if(m < sqrt_tol){
-        // A&S 16.13.4
-        return u - 0.25*m*(u - 0.5*sin(2.0*u));
-    }
-    float m1 = 1. - m;
-    if(m1 < sqrt_tol){
-        // A&S 16.15.4
-        float t = tanh(u);
-        return asin(t) + 0.25*m1*(t - u*(1. - pow(t, 2.)))*cosh(u);
-    }
-
-    float a = 1.;
-    float b = sqrt(m1);
-    float c = sqrt(m);
-    int n = 0;
-    while(abs(c) > tol){
-        if(n>=10){ return 1.0/0.0;}
-        float atemp = 0.5*(a+b);
-        float btemp = sqrt(a*b); 
-        float ctemp = 0.5*(a-b);
-        a = atemp;
-        b = btemp;
-        c = ctemp;
-        ambuf[n] = c/a;
-        n = n + 1;
-    }
-
-    float phi = a*u*pow(2.,float(n));
-    for(int i = n-1; i >= 0; i--){
-        phi = 0.5*(phi + asin(ambuf[i]*sin(phi)));
-    }
-    return phi;
-}
-
-float sn(float u, float m){
-    bool lt0 = m < 0.;
-    bool gt1 = m > 1.;
-    if (!(lt0) && !(gt1)){
-        float phi = am(u, m, D1MACH3*2.);
-        return sin(phi);
-    } else if(lt0) {
-        float mu1 = 1.0/(1.-m);
-        float mu = -m*mu1;
-        float sqrtmu1 = sqrt(mu1);
-        float v = u/sqrtmu1;
-        float phi = am(v, mu, D1MACH3*2.);
-
-        float s = sin(phi);
-
-        return sqrtmu1*s/sqrt(1.-mu*pow(s,2.));
-
-    } else {
-        float mu = 1. / m;
-        float v = u * sqrt(m);
-        float phi = am(v, mu, D1MACH3*2.);
-
-        return sqrt(mu)*sin(phi);
-    }
-}
-
-float cn(float u, float m){
-    bool lt0 = m < 0.;
-    bool gt1 = m > 1.;
-    if (!(lt0) && !(gt1)){
-        float phi = am(u, m, D1MACH3*2.);
-        return cos(phi);
-    } else if(lt0) {
-        float mu1 = 1.0/(1.-m);
-        float mu = -m*mu1;
-        float sqrtmu1 = sqrt(mu1);
-        float v = u/sqrtmu1;
-        float phi = am(v, mu, D1MACH3*2.);
-
-        float s = sin(phi);
-
-        return cos(phi)/sqrt(1.-mu*pow(s,2.));
-
-    } else {
-        float mu = 1. / m;
-        float v = u * sqrt(m);
-        float phi = am(v, mu, D1MACH3*2.);
-
-        return sqrt(1.- mu*pow(sin(phi),2.));
-    }
-}
-
-float rsin(float mag, float psi){
-    vec2 q = vec2(2.*mag*mag, 0.);
-    vec2 p = vec2(-mag*mag, 0.);
-    vec2 C1 = c_pow(-q/2. + c_pow(c_pow(q, 2.)/4. + c_pow(p, 3.)/27., 1./2.), 1./3.);
-    vec2 C2 = c_m(C1, vec2(-1./2., sqrt(3.)/2.));
-    vec2 C3 = c_m(C1, vec2(-1./2., -sqrt(3.)/2.));
-    vec2 v4 = C1 - c_d(p, 3.*C1);
-    vec2 v1 = C2 - c_d(p, 3.*C2);
-    vec2 v3 = C3 - c_d(p, 3.*C3);
-
-    vec2 v32 = v3;
-    vec2 v21 = -v1;
-    vec2 v41 = v4 - v1;
-    vec2 v31 = v3 - v1;
-    vec2 v42 = v4;
-
-
-    float A = pow(c_m(v32, v42)[0], 0.5);
-    float B = pow(c_m(v31, v41)[0], 0.5);
-    
-    
-    float arg = sqrt(A*B)*(psi)/mag;
-    if(mag*mag < 27.){
-        float ellk = (pow(A + B, 2.) - pow(v1[0], 2.)) / (4.*A*B);
-        float fo =F(acos((A-B)/(A+B)), ellk) ;
-        if(arg < fo ){
-            float can = cn(fo - arg, ellk);
-            float num = -A*v1[0] + (A*v1[0])*can;
-            float den = -A + B + (A+B)*can;
-            return num/den;
-        } else {return 0.0;}
-    } else {
-
-        float ellk = v32[0]*v41[0] / (v31[0]*v42[0]);
-        float fo = F(asin(sqrt(v31[0]/v41[0])), ellk);
-        if(arg < 4.*fo ){
-            float san = v41[0]*pow(sn(fo - sqrt(v31[0]*v42[0])*psi/(2.*mag), ellk), 2.0);
-            float num = v31[0]*v4[0]-v3[0]*san;
-            float den = v31[0]-san;
-            return num/den;
-        } else {return 0.0;}
-    }
-}
-
 float psimax(float mag){
     vec2 q = vec2(2.*mag*mag, 0.);
     vec2 p = vec2(-mag*mag, 0.);
@@ -256,71 +118,13 @@ float psimax(float mag){
     vec2 v31 = v3 - v1;
     vec2 v42 = v4;
 
-
-    
     float ellk = v32[0]*v41[0] / (v31[0]*v42[0]);
     return 4.*mag*F(asin(sqrt(v31[0]/v41[0])), ellk)/sqrt(v31[0]*v42[0]);
 }
 
-float rsout(float mag, float psi){
-//Created by FabriceNeyret2 in 2019-02-14  https://www.shadertoy.com/view/3d23Dc
-    // --- Solving a*x³ +b*x³ +c*x +d = 0. -> l = min(3 solutions)
-    float a = 1., b = 0., c = -mag*mag, d = 2.*mag*mag,
-        Q, A, D, V, l, k = -1.;
-
-    if (abs(a)<1e-3) {                          // degenerated P3
-        k = -2.;
-        V = c*c - 4.*b*d;
-        l = ( -c -sign(b)* sqrt(V) ) / (2.*b);
-    }
-    else {                                      // true P3       
-    b /= a; c /= a; d /= a;
-    float p = ( 3.*c - b*b ) / 3.,
-            q = ( 9.*b*c - 27.*d - 2.*b*b*b) / 27., // -
-            r = q/2.; Q = p/3.;
-            D = Q*Q*Q + r*r;
-    
-    if ( D < 0.) {                            // --- if 3 sol
-        A = acos(r/sqrt(-Q*Q*Q)), 
-        k = round(1.5-A/6.283); // k = 0,1,2 ; we want min l
-#define sol(k) ( 2.*sqrt(-Q)* cos((A+(k)*6.283)/3.) - b/3. )
-        l = sol(k);
-    }
-    else                                        // --- if 1 sol
-        if (p>0.) V = -2.*sqrt(p/3.), 
-#define asinh(z) ( sign(z)*asinh(abs(z)) )      // fix asinh() symetry error 
-                l = -V* sinh(asinh(3.*-q/p/V)/3.) -b/3.; 
-        else      V = -2.*sqrt(-p/3.), 
-                l = sign(-q)*V* cosh(acosh(3.*abs(q)/p/V)/3.) -b/3.;
-    }
-    
-    // --- display
-    float r1 = l;
-    float r3 = sol(k+1.);
-    float r4 = sol(k+2.);
-    r3 = min(r4, r3);
-    r4 = max(r4, r3);
-
-    float r32 = r3;
-    float r41 = r4 - r1;
-    float r31 = r3 - r1;
-    float r42 = r4;
-    
-            
-
-    float ellk = min(r32*r41 / (r31*r42), 1.0);
-    float fo = F(asin(sqrt(r31/r41)),ellk);
-    float san = r41 * pow(sn(fo - 1./2. * sqrt(r31*r42)*psi/(mag), ellk), 2.0);
-    return (r31*r4-r3*san) / (r31 - san) ;
-
-}
-
-
-
-
 void main() {
     float scale = 15.0; // size of disk
-    float scale2 = 40.;//size of horizon
+    float scale2 = 40.; //size of horizon
     vec2 uv = 2. * scale2 * ((gl_FragCoord.xy ) / uResolution.xy - vec2(0.5 ,0.5)); 
     float x = uv.x;
     float y = uv.y;
@@ -329,67 +133,24 @@ void main() {
     float costheta = cos(theta);
     float sinvarphi = sign(costheta)*y/mag;
 
-
-
     float tanvarphi = sinvarphi/abs(cosvarphi);
     float psi = acos(-((sin(theta)*tanvarphi) / 
         (pow(pow(costheta,2.0) + pow(tanvarphi,2.0), .5))));
 
-    float rs = 0.0;
-    float rs1 = 0.0;
-    //float rs2 = 0.0;
-
-    //gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
     float phi = M_PI/2.*(1.+sign(costheta)) + M_PI*(1.-sign(y)) + sign(y)*acos(costheta*cosvarphi/sqrt(1.0-pow(sin(theta)*cosvarphi, 2.0)));
     float phi2 = phi + M_PI;
 
-
     if (mag*mag > 27.){
-        rs = rsin(mag, psi);
-        rs1 = rsin(mag, M_PI+ psi);
-        //rs2 = rsin(mag, 2.*M_PI + psi);
-
 
         float deltapsi = psimax(mag) - M_PI;
         vec2 texcrd = (gl_FragCoord.xy/uResolution.xy - vec2(0.5, 0.5));
         float texcrd2rad = length(texcrd);
         float new_length = tan(atan(texcrd2rad)-deltapsi)/(texcrd2rad);
         vec2 texcrd3 = new_length*texcrd/vec2(1.,3.) + vec2(0.5, 0.5+theta/(2.*M_PI));
-        //vec2 texcrd3 = new_length*texcrd/scale + vec2(0.5, 0.5+theta;
         texcrd3 = vec2(texcrd3[0]- floor(texcrd3[0]), texcrd3[1]- floor(texcrd3[1]));
         gl_FragColor = texture2D(textureft, texcrd3);
-        //gl_FragColor = vec4(sin(deltapsi));
-
-
         
     } else {
-        rs = rsin(mag, psi);
-        rs1 = rsin(mag, M_PI + psi);
         gl_FragColor = vec4(0., 0., 0., 1.);
     }
-    
-    if(rs < 2.0){
-        gl_FragColor = vec4(0., 0., 0., 1.);
-        return;
-    }
-    
-    if (rs > 7.0 && rs < scale) {
-        vec2 uv2 = rs*vec2(cos(phi),sin(phi))/(2.0*scale)  + vec2(0.5, 0.5) ;
-        //gl_FragColor = texture2D(texture1, uv2);
-
-    }
-    if (rs1 > 7.0 && rs1 < scale){
-        vec2 uv3 = rs1*vec2(cos(phi),sin(phi))/(2.0*scale)  + vec2(0.5, 0.5) ;
-        //gl_FragColor += texture2D(texture1, uv3);
-    }
-    //if (rs2 > 7.0 && rs2 < scale){
-    //    vec2 uv4 = rs2*vec2(cos(phi),sin(phi))/(2.0*scale)  + vec2(0.5, 0.5) ;
-    //    gl_FragColor += texture2D(texture1, uv4);
-    //}
-
-    
-
-
-
-    //gl_FragColor = vec4(0.,0.,0.,1.);texture2D(textureft, uv);
 }
--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 import Stats from '../node_modules/stats-js/src/Stats.js';
 
-var renderer, uniforms, vShader, fShader, vSkyShader, fSkyShader, camera, scene, acc_disk, skyboxGeo, skyBox, theta, stats, video;
+var renderer, uniforms, vShader, fShader, camera, scene, screen, stats, video;
 var loader = new THREE.FileLoader();
 init();
 
@@ -22,7 +22,7 @@ function init() {
 
     scene = new THREE.Scene();
 
-    var numFilesLeft = 4;
+    var numFilesLeft = 2;
 
     function runMoreIfDone() {
         --numFilesLeft;
@@ -56,46 +56,20 @@ function init() {
 
     loader.load('../src/fragment.glsl', function ( data ) {fShader =  data; runMoreIfDone(); },);
     loader.load('../src/vertex.glsl', function ( data ) {vShader =  data; runMoreIfDone(); },);
-    loader.load('../src/skyfragment.glsl', function ( data ) {fSkyShader =  data; runMoreIfDone(); },);
-    loader.load('../src/skyvertex.glsl', function ( data ) {vSkyShader =  data; runMoreIfDone(); },);
 
 }
 
 function more() {
     var geometry = new THREE.PlaneGeometry(2, 2);
 
-    //var texture = new THREE.TextureLoader().load('static/space.png')
-    //var texture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/d/d3/Albert_Einstein_Head.jpg')//.load('space.png')
-    //var texture = new THREE.TextureLoader().load('https://images.theconversation.com/files/393213/original/file-20210401-13-1w9xb24.jpg?ixlib=rb-1.1.0&q=30&auto=format&w=600&h=400&fit=crop&dpr=2')//https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/The_Event_Horizon_Telescope_and_Global_mm-VLBI_Array_on_the_Earth.jpg/1920px-The_Event_Horizon_Telescope_and_Global_mm-VLBI_Array_on_the_Earth.jpg')//.load('space.png')
-
     // VIDEO
     
     var texture2 = new THREE.VideoTexture(video);
-    //var texture2 = new THREE.TextureLoader().load('../public/images/eso0932a.png')//.load('space.png')
-    //var texture1 = new THREE.TextureLoader().load('https://cdn.vox-cdn.com/thumbor/nPHkBUDla9JcRJWRdswdAETz4MU=/0x0:1960x2000/1820x1213/filters:focal(686x574:998x886):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/71122307/STScI_01G7PWWPY7XRR9PW95W9W8ZYZW.0.png')
     var texture1 = new THREE.TextureLoader().load('../public/images/clouds.jpeg')
-    //var texture2 = new THREE.TextureLoader().load('https://cdn-icons-png.flaticon.com/512/25/25435.png')
-
-    //const ft = new THREE.TextureLoader().load("../public/images/ulukai/corona_ft.png");
-    //const ft = new THREE.TextureLoader().load("https://upload.wikimedia.org/wikipedia/commons/3/34/Gnomonic_projection_SW.jpg")
-    //const ft = new THREE.TextureLoader().load("https://tomassobekphotography.co.nz/photos/_data/i/upload/2018/08/15/20180815085151-4d7b709e-me.jpg")
-    const ft = new THREE.TextureLoader().load("../public/images/milkyway.jpg")
-    const bk = new THREE.TextureLoader().load("../public/images/ulukai/corona_bk.png");
-    const up = new THREE.TextureLoader().load("../public/images/ulukai/corona_up.png");
-    const dn = new THREE.TextureLoader().load("../public/images/ulukai/corona_dn.png");
-    const rt = new THREE.TextureLoader().load("../public/images/ulukai/corona_rt.png");
-    const lf = new THREE.TextureLoader().load("../public/images/ulukai/corona_lf.png");
-
 
     uniforms = {
-    theta :    {value: 0},
     texture1:  {value:texture1},
     textureft: {value:texture2},
-    texturebk: {value:bk},
-    textureup: {value:up},
-    texturedn: {value:dn},
-    texturert: {value:rt},
-    texturelf: {value:lf},
     uResolution: {
         value: new THREE.Vector2(800, 800),
     },
@@ -107,61 +81,20 @@ function more() {
     blending:       THREE.AdditiveBlendMode,
     transparent:    true
     });
-    var reflective_material = new THREE.MeshBasicMaterial(0xffffff);
-    acc_disk = new THREE.Mesh(geometry, shader_material);
+    screen = new THREE.Mesh(geometry, shader_material);
 
-    acc_disk.position.z = -1;
-    scene.add(acc_disk);
+    screen.position.z = -1;
+    scene.add(screen);
 
-
-    
-    const texture_arr = [ft, bk, up, dn, rt, lf]
-    //const materialArray = texture_arr.map(texture => {return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide })})
-
-    //skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
-    //skyBox = new THREE.Mesh(skyboxGeo, materialArray);
-    ////skyBox.position.set(1200, -250, -20000);
-    //scene.add(skyBox);
-    //var skygeometry = new THREE.SphereGeometry(1000, 60, 40);  
-    //var skyuniforms = {  
-    //    texture1: { type: 't', value: new THREE.TextureLoader().load('https://cdn.eso.org/images/screen/eso0932a.jpg') }
-    //};
-    //var skyMaterial = new THREE.ShaderMaterial( {  
-    //    uniforms:       skyuniforms,
-    //    vertexShader:   vSkyShader,
-    //    fragmentShader: fSkyShader,
-    //    side:           THREE.BackSide
-    //    //blending:       THREE.AdditiveBlendMode,
-    //    //transparent:    true
-
-    //});
-
-    //skyBox = new THREE.Mesh(skygeometry, skyMaterial);  
-    //skyBox.scale.set(-1, 1, 1);  
-    ////skyBox.position.set(0, 0, -20);
-    //skyBox.eulerOrder = 'XZY';  
-    ////skyBox.renderDepth = 1000.0;  
-    //scene.add(skyBox);  
-
-
-    theta = 85*Math.PI/180;
     stats = new Stats();
     stats.showPanel( 1 ); // 0: fps, 1: ms, 2: mb, 3+: custom
     document.body.appendChild( stats.dom );
-
 
     animate();
 }
 
 function animate(){
     stats.begin();
-    var d = new Date();
-    theta = d.getTime()/5000 % 2*Math.PI
-    if (Math.abs(theta - Math.PI/2.) < 0.005 || Math.abs(theta - 3.*Math.PI/2.) < 0.005){
-        theta += 0.01;
-    }
-    //skyBox.rotation.x = 2*Math.PI-theta;
-    //acc_disk.material.uniforms.theta.value = theta;//  + 3.14 * (Math.abs(Math.sin(theta)))/2.;
     renderer.render(scene, camera)
     stats.end();
     requestAnimationFrame(animate);
